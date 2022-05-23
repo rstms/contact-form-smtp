00148e92da3546536e1e9a35090efd069837e5af
Matt Krueger <mkrueger@rstms.net>
2022-05-23 05:21:24 -0500

    add diagnostic output

--- contact_form_smtp/app.py
+++ contact_form_smtp/app.py
@@ -8,4 +8,6 @@ app = FastAPI()
 @app.post("/contact/send")
 async def post_contact_send(request: Request):
     print(f"{request=}")
+    print(f"{request.query_params=}")
+    print(f"{request.path_params=}")
     return {"status": "ok"}
