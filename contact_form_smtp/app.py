89d153817c79a14e3fa0a7f053006fa244d3ea65
Matt Krueger <mkrueger@rstms.net>
2022-05-23 07:20:41 -0500

    functional

--- contact_form_smtp/app.py
+++ contact_form_smtp/app.py
@@ -1,15 +1,34 @@
 # contactform app
 
+import datetime
+from smtplib import SMTP
+
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.post("/contact/send")
-async def post_contact_send(request: Request):
-    print(f"{request=}")
-    form_data = await request.form()
-    print(f"{form_data=}")
-    json_data = await request.json()
-    print(f"{json_data=}")
-    return {"status": "ok"}
+async def post_contact_send(request: Request, status_code=201):
+    message = await request.json()
+    from_addr = message["From"]
+    to_addr = message["To"]
+    subject = message["Subject"]
+    body = message["HtmlBody"]
+
+    body = body.replace("<div>", "")
+    body = body.replace("</div>", "\n\n")
+    message_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    with SMTP("smtp") as smtp:
+        smtp.set_debuglevel(1)
+        # smtp.connect('smtp', 25)
+        msg = (
+            f"From: {from_addr}\n"
+            f"To: {to_addr}\n"
+            f"Subject: {subject}\n"
+            f"Date: {message_date}\n"
+            "\n"
+            f"{body}"
+        )
+        smtp.sendmail(from_addr, to_addr, msg)
--- make.include/lint.mk
+++ make.include/lint.mk
@@ -4,11 +4,11 @@ lint_src = $(module) tests docs
 
 # blacken python source (code formatter)
 fmt: lint
-	isort $(lint_src)
-	black $(lint_src)
 
 # check style, lint with flake8
 lint:
+	isort $(lint_src)
+	black $(lint_src)
 	flake8 --config tox.ini $(lint_src)
 
 # vim:ft=make
