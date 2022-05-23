a0b154863252d99fdb970ee011f21cdd331638fd
Matt Krueger <mkrueger@rstms.net>
2022-05-23 05:05:41 -0500

    Bump version: 0.1.5 → 0.1.6

--- contact_form_smtp/version.py
+++ contact_form_smtp/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-05-23 05:03:29-05:00"
+__timestamp__ = "2022-05-23 05:05:41-05:00"
